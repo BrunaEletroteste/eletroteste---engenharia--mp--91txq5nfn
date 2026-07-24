21415009381e5068d7a8937f58d3f3466a0c3dde
Skip <skip@usecurling.com>
2026-07-23 22:02:56 -0300

    Fix title line break for preventive/corrective reports

--- src/components/reports/pdf/PDFFormatters.tsx
+++ src/components/reports/pdf/PDFFormatters.tsx
@@ -113,9 +113,9 @@ export const formatDate = (dateStr: string) =>
 export const renderLaudoTitle = (tipo?: string) =>
   tipo === 'PREVENTIVA_CORRETIVA' ? (
     <React.Fragment>
-      LAUDO TÉCNICO DE MANUTENÇÃO PREVENTIVA
+      {'LAUDO TÉCNICO DE MANUTENÇÃO PREVENTIVA '}
       <br />
-      E CORRETIVA EM CABINE(S) PRIMÁRIA(S)
+      {'E CORRETIVA EM CABINE(S) PRIMÁRIA(S)'}
     </React.Fragment>
   ) : (
     'LAUDO TÉCNICO DE MANUTENÇÃO PREVENTIVA EM CABINE(S) PRIMÁRIA(S)'
